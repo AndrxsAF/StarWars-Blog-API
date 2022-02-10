b84bc4d3fe10b8f7de1e5e8ebceea1abc25fc1ae
AndrxsAF <andrxsaf@gmail.com>
2022-02-10 16:57:58 +0000

    featuring database and endpoints

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, People, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,172 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# functions for planet
+
+# functions to show the user database
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def show_users():
+    users = User.query.all()
+    return jsonify(list(map(lambda tasks: tasks.serialize(), users))), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# functions to add a new user to database
+    
+@app.route('/user/add', methods=['POST'])
+def add_users():
+    body = request.get_json()
+    new_user = User(username=body["username"])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 200
 
-    return jsonify(response_body), 200
+# functions to show or delete an user from the database
+
+@app.route('/user/<id>', methods=['GET', 'DELETE'])
+def delete_users(id):
+    user = User.query.get(id)
+    if user:
+        if request.method == "GET":
+            return jsonify(user.serialize())
+        elif request.method == "DELETE":
+            db.session.delete(user)
+            db.session.commit()
+            return jsonify('User deleted successfuly.')
+    else:
+        return jsonify('No existe esa función')
+
+# functions for favorites
+
+# functions to add and delete a planet in favorites
+
+@app.route('/user/<id>/favorites/planet/<planetID>', methods=['POST', 'DELETE'])
+def add_favorite_planet(id, planetID):
+    if request.method == "POST":
+        new_favorite = Favorite(userID = id, planetID = planetID, peopleID = None)
+        db.session.add(new_favorite)
+        db.session.commit()
+        return jsonify(new_favorite.serialize()), 200
+    elif request.method == "DELETE":
+        favorites = Favorite.query.all()
+        favorite_list = list(map(lambda favorite: favorite.serialize(), favorites))
+        for x in favorite_list:
+            if x.get('userID') == int(id) and x.get('planetID') == int(planetID):
+                delete_favorite = Favorite.query.get(int(x.get('id')))
+                db.session.delete(delete_favorite)
+                db.session.commit()
+                return jsonify(delete_favorite.serialize())    
+        return jsonify('Not correct.')
+    else:
+        return jsonify('Only POST and DELETE methods allowed here.')
+
+# functions to add and delete a people in favorite
+
+@app.route('/user/<id>/favorites/people/<peopleID>', methods=['POST', 'DELETE'])
+def add_favorites_people(id, peopleID):
+        if request.method == "POST":
+            new_favorite = Favorite(userID = id, planetID = None, peopleID = peopleID)
+            db.session.add(new_favorite)
+            db.session.commit()
+            return jsonify(new_favorite.serialize()), 200
+        elif request.method == "DELETE":
+            favorites = Favorite.query.all()
+            favorite_list = list(map(lambda favorite: favorite.serialize(), favorites))
+            for x in favorite_list:
+                if x.get('userID') == int(id) and x.get('peopleID') == int(peopleID):
+                    delete_favorite = Favorite.query.get(int(x.get('id')))
+                    db.session.delete(delete_favorite)
+                    db.session.commit()
+                    return jsonify(delete_favorite.serialize())
+            return jsonify('Not correct.')
+        else:
+            return jsonify('Only POST and DELETE methods allowed here.')
+        
+# functions to show a user favorites
+
+@app.route('/user/<id>/favorites', methods=['GET'])
+def user_favorites(id):
+    favorites = Favorite.query.all()
+    favorite_list = list(map(lambda favorite: favorite.serialize(), favorites))
+    user_favorite = []
+    for x in favorite_list:
+        if x.get('userID') == int(id):
+            user_favorite.append(x)
+    return jsonify(list(map(lambda favorite: favorite, user_favorite))), 200
+
+# functions to show the favorite database
+
+@app.route('/favorite', methods=['GET'])
+def show_favorite():
+    favorites = Favorite.query.all()
+    return jsonify(list(map(lambda favorite: favorite.serialize(), favorites))), 200
+
+# functions for planet
+
+# functions to show the planet database
+
+@app.route('/planet', methods=['GET'])
+def show_planets():
+    planets = Planet.query.all()
+    return jsonify(list(map(lambda planet: planet.serialize(), planets))), 200
+
+# functions to add a new planet to database
+    
+@app.route('/planet/add', methods=['POST'])
+def add_planets():
+    body = request.get_json()
+    new_planet = Planet(name=body["name"], terrain=body["terrain"], population=body["population"])
+    db.session.add(new_planet)
+    db.session.commit()
+    return jsonify(new_planet.serialize()), 200
+
+# functions to show or delete a planet from the database
+
+@app.route('/planet/<id>', methods=['GET', 'DELETE'])
+def delete_planets(id):
+    planets = Planet.query.get(id)
+    if planets:
+        if request.method == "GET":
+            return jsonify(planets.serialize())
+        elif request.method == "DELETE":
+            db.session.delete(planets)
+            db.session.commit()
+            return jsonify('Planet deleted successfuly.')
+    else:
+        return jsonify('No existe esa función')
+
+# functions for people
+
+# functions to show the people database
+
+@app.route('/people', methods=['GET'])
+def show_peoples():
+    peoples = People.query.all()
+    return jsonify(list(map(lambda people: people.serialize(), peoples))), 200
+
+# functions to add a new people to database
+    
+@app.route('/people/add', methods=['POST'])
+def add_peoples():
+    body = request.get_json()
+    new_people = People(name=body["name"], height=body["height"], mass=body["mass"], homeworld=body["homeworld"])
+    db.session.add(new_people)
+    db.session.commit()
+    return jsonify(new_people.serialize()), 200
+
+# functions to show or delete a people from the database
+
+@app.route('/people/<id>', methods=['GET', 'DELETE'])
+def delete_peoples(id):
+    peoples = People.query.get(id)
+    if peoples:
+        if request.method == "GET":
+            return jsonify(peoples.serialize())
+        elif request.method == "DELETE":
+            db.session.delete(peoples)
+            db.session.commit()
+            return jsonify('People deleted successfuly.')
+    else:
+        return jsonify('No existe esa función')
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,17 +3,79 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    __tablename__ = 'user'
+    userID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.userID
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
+            "userID": self.userID,
+            "username": self.username
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    planetID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    terrain = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.planetID
+
+    def serialize(self):
+        return {
+            "planetID": self.planetID,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    peopleID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    homeworld = db.Column(db.Integer, db.ForeignKey('planet.planetID'))
+    db.planet = db.relationship('Planet', backref='people')
+
+    def __repr__(self):
+        return '<People %r>' % self.peopleID
+
+    def serialize(self):
+        return {
+            "peopleID": self.peopleID,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "homeworld": self.homeworld
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('user.userID'))
+    planetID = db.Column(db.Integer, db.ForeignKey('planet.planetID'))
+    peopleID = db.Column(db.Integer, db.ForeignKey('people.peopleID'))
+    user = db.relationship('User', backref='favorite')
+    planet = db.relationship('Planet', backref='favorite')
+    people = db.relationship('People', backref='favorite')
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "userID": self.userID,
+            "planetID": self.planetID,
+            "peopleID": self.peopleID,
+            "id": self.id
             # do not serialize the password, its a security breach
         }
